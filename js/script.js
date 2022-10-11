4313c55594b6abcbe82dec11db710d54024f1be5
Stinkin <stinkinevil@hotmail.com>
2022-10-11 20:11:04 -0300

    Cambio completo del sitio

---
+++ js/script.js
@@ -0,0 +1,38 @@
+document.getElementById("head").innerHTML = `
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Iconos-->
+<link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+<link rel="shortcut icon" href="img/shortcut.ico" type="image/x-icon">
+
+<!-- Hojas de estilo principales-->
+<link rel="stylesheet" href="CSS/style.css">
+<link rel="stylesheet" href="CSS/navbar.css">
+<link rel="stylesheet" href="CSS/responsive.css">
+
+<!-- Este es para hacer las pruebas de CSS -->
+<link rel="stylesheet" href="CSS/TEST.css">
+
+<title>PROYECTO HTML, CSS, JS</title>
+`
+
+document.getElementById("header").innerHTML = 
+`
+<div>
+<H1>LENGUAJES DE PROGRAMACIÓN</H1>
+</div>
+`
+
+
+document.getElementById("footer").innerHTML = `
+<div>
+<a class="social FB" href="http://facebook.com" target="_blank"><i class='bx bxl-facebook'></i></a>
+<a class="social WA" href="http://web.whatsapp.com" target="_blank"><i class='bx bxl-whatsapp'></i></a>
+<a class="social IG" href="http://www.instagram.com" target="_blank"><i class='bx bxl-instagram'></i></a>
+<a class="social TT" href="http://tiktok.com" target="_blank"> <i class='bx bxl-tiktok'></i></a>
+<br>
+&copy; Derechos Reservados 2022 
+</div>
+`
+
